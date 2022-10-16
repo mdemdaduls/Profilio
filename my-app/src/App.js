d9104dc050ba3cea6c40c4bd107718b4f719d66b
unknown <mdehshawonu@gmaile.com>
2022-10-16 19:40:39 +0600

    Done the Projects

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <product></product>
+    
+     
+    </div>
+  );
+}
+function product(){
+  return(
+    <div>
+      <h1>Name:{}</h1>
+      <p>Price:{}</p>
+    </div>
+  )
+}
+export default App;
